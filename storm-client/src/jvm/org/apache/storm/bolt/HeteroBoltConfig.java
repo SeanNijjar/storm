a005c50dffd476bdcb29fe8c3afab0220275b7af
SeanNijjar <sean.nijjar@gmail.com>
2019-07-04 18:34:07 -0400

    Get build working (some library version changes)

--- storm-client/src/jvm/org/apache/storm/bolt/HeteroBoltConfig.java
+++ storm-client/src/jvm/org/apache/storm/bolt/HeteroBoltConfig.java
@@ -1,4 +1,25 @@
 package org.apache.storm.bolt;
 
+import org.apache.storm.daemon.worker.WorkerState;
+
+import java.util.concurrent.locks.Lock;
+
 public class HeteroBoltConfig {
+    /*
+    WorkerState workerStateHook;
+
+    private static HeteroBoltConfig heteroBoltConfigSingleton = null;
+
+    private HeteroBoltConfig(WorkerState _workerStateHook) {
+        workerStateHook = _workerStateHook;
+
+    }
+
+    public static HeteroBoltConfig get() {
+        if (heteroBoltConfigSingleton == null) {
+            Lock
+            heteroBoltConfigSingleton
+        }
+    }
+    */
 }
